2622a9f9c5752237fb3b221e510d554f7fe7a075
Amir Rezaie <amirrezaie.dev@gmail.com>
2023-02-26 19:42:53 +0330

    registration.php and registration class created

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+
+class  Registration
+{
+    private  $id;
+    private $name;
+
+    public function __construct()
+    {
+
+    }
+
+    public static function add()
+    {
+        echo "Add";
+    }
+
+}
